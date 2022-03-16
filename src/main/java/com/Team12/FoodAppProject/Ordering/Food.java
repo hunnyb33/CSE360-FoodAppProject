d66f6ed9a59b9caaa001e85d6fb782dc42dcf822
hunnyb33 <krgray6@asu.edu>
2022-03-16 15:31:13 -0700

    completed sufficent work on Food.java and Order.java classes before the meeting on 03/16, need to discuss more in meeting

--- src/main/java/com/Team12/FoodAppProject/Ordering/Food.java
+++ src/main/java/com/Team12/FoodAppProject/Ordering/Food.java
@@ -1,14 +1,10 @@
 package com.Team12.FoodAppProject.Ordering;
 
 import javafx.scene.image.Image;
-
+import java.io.Serializable;
 import java.util.ArrayList;
 
-/*
- * NOTE: This class is temporary as I need to wait for the proper one to be made by Kyle.
- * */
-
-public class Food {
+public class Food implements Seriablizable {
 
 	private String name;
 	private double price;
@@ -20,7 +16,14 @@ public class Food {
 	// cannot have a default food item
 	private Food() {}
 
-	// custom constructor for Food()
+	/** Constructor for a Food object
+	* @param name name of this new food item as a string
+	* @param price price of this food as a double
+	* @param timeToCook time it takes for this food item to be made once it starts
+	* @param tags tags for this food item as stored in an array list, java.util.ArrayList
+	* @param image image for this item, Image as defined in javafx.scene.image.Image
+	* @param description description for this food item as a string
+	*/
 	public Food(String name, double price, int timeToCook, ArrayList<String> tags, Image image, String description) {
 
 		this.name = name;
@@ -32,4 +35,146 @@ public class Food {
 
 	} // end of custom constrcutor for Food() item
 
-}
+	/** Accessor for name of this item
+	*/
+	public String getName() {
+
+		return this.name;
+
+	} // end of getName() method defintion
+
+	/** Accessor for price of this item
+	*/
+	public double getPrice() {
+
+		return this.price;
+
+	} // end of getPrice() method defintion
+
+	/** Accessor for time to cook of this item
+	*/
+	public int getTimeToCook() {
+
+		return this.timeToCook();
+
+	} // end of getTimeToCook() method defintion
+
+	/** for the tags of this item
+	* @return java.util.ArrayList
+	*/
+	public List<String> getTags() {
+
+		return this.tags;
+
+	} // end of getTags() method defintion
+
+	/** Accessor for the image of this item
+	* @return javafx.scene.image.Image
+	*/
+	public Image getImage() {
+
+		return this.image;
+
+	} // end of getImage() function defintion
+
+	/** Accessor for the desription of this item
+	*/
+	public String getDescription() {
+
+		return this.description;
+
+	} // end of getDestription() function defintion
+
+	/** Sets the name of this food item
+	* <p> All mutators in this class should only be accessed by the Management class
+	*/
+	public void setName(String name) {
+
+		this.name = name;
+
+	} // end of setName() function  defintion
+
+	/** Set the price for this food item
+	* <p> All mutators in this class should only be accessed by the Management class
+	*/
+	public void setPrice(double price) {
+
+		this.price = price;
+
+	} // end of setPrice() method defintion
+
+	/** Sets the time to cook for this food item
+	* <p> All mutators in this class should only be accessed by the Management class
+	*/
+	public void setTimeToCook(int timeToCook) {
+
+		this.timeToCook = timeToCook;
+
+	} // end of setTimeToCook() method defintion
+
+	/** Do we want this function or replace with just addTag() and removeTag() functions?
+	*/
+	public void setTags(List<String> tags) {
+
+		// work in progress
+
+	} // end of setTags() function
+
+	/** Sests the image for this food item
+	* <p> All mutators on this class should only be accessed by the Management class
+	* @param image is from javafx.scene.image.Image
+	*/
+	public void setImage(Image image) {
+
+		this.image = image;
+
+	} // end of setImage() function
+
+	/** Sets the description for this food item
+	* <p> All mutators in this class should only be accessed by the Management class
+	*/
+	public void setDescription(String description) {
+
+		this.description = description;
+
+	} // end of setDescription() function
+
+	/** Checks if 2 foods are the same only by comaring the name variable
+	* <p> For most applications outside of management this is acceptable
+	* @param item1 first item as a Food object
+	* @param item2 second item, Food object
+	* @return true if the names match, false if otherwise
+	*/
+	public boolean compare(Food item1, Food item2) {
+
+		// checking for string equality
+		if (stringComapre(item1.getName(), item2.getName()) == 0) {
+
+			return true;
+
+		} // end of if they are the same block
+
+		// else return false
+		else {
+
+			return false;
+
+		} // end of else return false
+
+	} // end of compare() method defintion
+
+	/** Checks if 2 food items are the same by compare all variables in a Food object
+	* <p> This is for adding food items to the menu by management
+	* <p> Work in progress, will alway return true for now
+	* @param item1 first item as a food object
+	* @param item2 first item as a food object
+	* @return true if all the variables mathc, false if otherwise
+	*/
+	public boolean strictCompare(Food item1, Food item2) {
+
+		// work in progress
+		return true;
+
+	} // end of strictCompare() method defintion
+
+} // end of Food class defintion
--- src/main/java/com/Team12/FoodAppProject/Ordering/Order.java
+++ src/main/java/com/Team12/FoodAppProject/Ordering/Order.java
@@ -1,17 +1,109 @@
 import java.util.ArrayList;
+import java.io.Serializable;
 
-class Order {
+class Order implements Serializable {
 
 	private <Food> orderedFood;
-	private int orderStatus;
+	private short orderStatus;
 
-	// default constructor for Food()
-	public Food() {
+	/** Default constuctor for Order class
+	* Creates an empty order
+	*/
+	public Order() {
 
 		orderedFood = new ArrayList<>();
 
+		// defualt status for new order, zero
+		short = 0;
+
 	} // end of Food() default constructor
 
-	
+	/** Adds a food item to this this Order
+	* @param addMe food item represented as a Food object to be added to the list
+	* @return always true, add() method from java.util.ArrayList returns void
+	*/
+	public boolean addFood(Food addMe) {
+
+		this.orderedFood.add(addMe);
+
+		return true;
+
+	} // end of addFood() method defintion
+
+	/** Remove a food item from thsi order
+	* @param removeMe food item to be deleted represented as a Food object
+	* @return true if remove successfully, false if item not found
+	*/
+	public boolean removeFood(Food removeMe) {
+
+		// seach list
+		for (int i = 0, i < this.orderedFood.size(), i++) {
+
+			// if found item
+			if (compare(removeMe, this.orderedFood[i]) {
+
+				this.orderedFood.remove(i];
+
+				return true;
+
+			}  // end of if found item
+
+		} // end of while i less than ordered food size
+
+		// else item not found
+		return false;
+
+	} // end of removeFood() method defintion
+
+	/** Sets the total price of this order
+	* <p>does not include tax as of yet
+	* @return the sum of all the prices in the order
+	*/
+	public double getTotalPrice() {
+
+		double sum = 0;
+
+		// getting each price
+		for (int i = 0, i < this.orderedFood.size(), i++) {
+
+			sum = sum + this.orderedFood[i].getPrice();
+
+		} // end of for i less than orderedFoodSize
+
+		return sum;
+
+	} // end of getTotalPrice() method defintion
+
+	/** Work in progress
+	* <p> Does this code return the sum of the times for all the items?
+	* <p> Should this function take into accound all the orders ahead in the queue?
+	*/
+	public int getTimeToCook() {
+
+		// work in progress
+		return 0;
+
+	} // end of getTimeToCook() method defintion
+
+	/** It is beyond the scope of this function to actually move into the order queue
+	* <p> This function only enumerates the short, after payment
+	*/
+	public void moveToStack() {
+
+		this.orderStatus = 1;
+
+	} // end of moveToStack() method defintion
+
+	/** Returns the orderStatus
+	* <p> 0 for pre-payment
+	* <p> 1 for post-paymeny, in-queue
+	* <p> 2 for in progress, being made
+	* <p> 3 for order completed
+	*/
+	public short getStatus() {
+
+		return this.orderStatus;
+
+	} // end of getStatus() function definition
 
 } // end of Order class defintion
