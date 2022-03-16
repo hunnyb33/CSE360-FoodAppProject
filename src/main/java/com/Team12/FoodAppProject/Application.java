8b37d08bbbfbc9b276e906e884900e90905953a9
josephH00 <12785608+josephH00@users.noreply.github.com>
2022-03-15 21:29:29 -0700

    Made Application.java a JavaFX based application

--- src/main/java/com/Team12/FoodAppProject/Application.java
+++ src/main/java/com/Team12/FoodAppProject/Application.java
@@ -1,7 +1,15 @@
 package com.Team12.FoodAppProject;
 
-public class Application {
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
         System.out.println("Hello World!");
     }
 }
