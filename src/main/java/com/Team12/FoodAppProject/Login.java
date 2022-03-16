ed1653e625b92b8539113128b78360b88db1d5d1
eltwento <trentbuckallew@gmail.com>
2022-03-16 15:52:17 -0700

    added login/signup files

---
+++ src/main/java/com/Team12/FoodAppProject/Login.java
@@ -0,0 +1,7 @@
+package com.Team12.FoodAppProject;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("This is the login page.");
+    }
+}
---
+++ src/main/java/com/Team12/FoodAppProject/SignUp.java
@@ -0,0 +1,7 @@
+package com.Team12.FoodAppProject;
+
+public class SignUp {
+    public static void main(String[] args) {
+        System.out.println("This is where you sign up.");
+    }
+}
