f45e6dadca4cb65f2c30c88b74a45b892ac36b9f
jessep13 <jessepurice@gmail.com>
2022-03-16 18:54:54 -0700

    Management (#2)
    * Implemented temporary classes and initiated the Management class.
    
    * Turned the User interface into an abstract class due to variable issues, and created constructor for Management.
    
    * Moved the Management package to proper folder
    
    * Changed Image from AWT to JavaFX
    
    * Made Food serializable in order to be able to be saved, and made a method in Management to save Food objects.
    
    * Implemented adding food items
    
    * Implemented finding food items for Management
    
    * Implemented deleting food items for Management

---
+++ src/main/java/com/Team12/FoodAppProject/management/Food.java
@@ -0,0 +1,29 @@
+package com.Team12.FoodAppProject.management;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javafx.scene.image.Image;
+
+/*
+ * NOTE: This class is temporary as I need to wait for the proper one to be made by Kyle.
+ * */
+
+public class Food implements Serializable
+{
+	String name;
+	double price;
+	int timeToCook;
+	ArrayList<String> tags;
+	Image image;
+	String description;
+	
+	public Food(String name, double price, int timeToCook, ArrayList<String> tags, Image image, String description)
+	{
+		this.name = name;
+		this.price = price;
+		this.timeToCook = timeToCook;
+		this.tags = tags;
+		this.image = image;
+		this.description = description;
+	}
+}
