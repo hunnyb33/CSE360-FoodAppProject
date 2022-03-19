e768e534c95e6b49ea92e02ec566ac47e8d891ab
agaarwalsarthak123 <64824548+agaarwalsarthak123@users.noreply.github.com>
2022-03-18 17:47:48 -0700

    Update Application.java

--- src/main/java/com/Team12/FoodAppProject/Application.java
+++ src/main/java/com/Team12/FoodAppProject/Application.java
@@ -1,7 +1,23 @@
 package com.Team12.FoodAppProject;
 
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) 
+    {
+        System.out.println("Press signup if you do not have an account");
+        System.out.println("Press Login if you alreayd have an account");
+    
+    }
+
+    bool signup
+    {
+        String firstName,lasttName, email;
+        float password;
+        System.out.println("Press Enter you First Name");
+        firstName = sc.nextLine();
+        System.out.println("Press Enter your last name");
+        System.out.println("Press Enter your email address");
+        System.out.println("Press Enter you password");
+        System.out.println("Re-Enter you password to confirm it");
+
     }
 }
