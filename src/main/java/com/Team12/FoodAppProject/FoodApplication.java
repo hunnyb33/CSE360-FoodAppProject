f2549e8a578c4a24b8695b046a7db3cc79b76389
Jesse Purice <jessepurice@gmail.com>
2022-03-22 11:00:41 -0700

    Added non-functional buttons to Management GUI

--- src/main/java/com/Team12/FoodAppProject/FoodApplication.java
+++ src/main/java/com/Team12/FoodAppProject/FoodApplication.java
@@ -1,6 +1,10 @@
 package com.Team12.FoodAppProject;
 
-import javafx.application.Application;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
 import javafx.stage.Stage;
 
 public class FoodApplication extends Application
@@ -9,6 +13,21 @@ public class FoodApplication extends Application
     public void start(Stage primaryStage) throws Exception
     {
         primaryStage.setTitle("Management GUI");
+        
+        // Main label
+        Label label = new Label("Welcome Management!\nHow would you like to adjust the menu?");
+        
+        // The three menu options
+        Button addFoodButton = new Button("Add new food item");
+        Button editFoodButton = new Button("Edit food item");
+        Button removeFoodButton = new Button("Remove food item");
+        
+        // Add buttons and label to VBox
+        VBox vBox = new VBox(label, addFoodButton, editFoodButton, removeFoodButton);
+        
+        // Create the management scene
+        Scene scene = new Scene(vBox, 800, 400);
+        primaryStage.setScene(scene);
         
         primaryStage.show();
     }
