3f75bb5fafed464c7b74b9f9ab4f85d509caedba
Jesse Purice <jessepurice@gmail.com>
2022-03-22 11:40:00 -0700

    Created the Add Food Item GUI.

--- src/main/java/com/Team12/FoodAppProject/FoodApplication.java
+++ src/main/java/com/Team12/FoodAppProject/FoodApplication.java
@@ -12,8 +12,6 @@ public class FoodApplication extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        primaryStage.setTitle("Management GUI");
-        
         // Main label
         Label label = new Label("Welcome Management!\nHow would you like to adjust the menu?");
         
@@ -22,14 +20,54 @@ public class FoodApplication extends Application
         Button editFoodButton = new Button("Edit food item");
         Button removeFoodButton = new Button("Remove food item");
         
+        // Set on actions
+        addFoodButton.setOnAction(actionEvent -> AddFoodItemWindow());
+        
         // Add buttons and label to VBox
         VBox vBox = new VBox(label, addFoodButton, editFoodButton, removeFoodButton);
         
         // Create the management scene
-        Scene scene = new Scene(vBox, 800, 400);
+        Scene scene = new Scene(vBox, 400, 200);
         primaryStage.setScene(scene);
-        
+        primaryStage.setTitle("Management GUI");
         primaryStage.show();
+    }
+    
+    private void AddFoodItemWindow()
+    {
+        // Main label
+        Label addFoodLabel = new Label("Add Food Item");
+        
+        // Text Fields
+        TextField nameTextField = new TextField("Item Name");
+        TextField priceTextField = new TextField("Item Price");
+        TextField timeTextField = new TextField("Item Time to Cook");
+        TextField tagTextField = new TextField("Item Tags");
+        
+        // Add Button for adding Images
+        Button imageButton = new Button("Add Item Image");
+        
+        // Add Confirm Button
+        Button confirmButton = new Button("Confirm New Item");
+        
+        // Add to VBox
+        VBox vBox = new VBox
+        (
+            addFoodLabel,
+            nameTextField,
+            priceTextField,
+            timeTextField,
+            tagTextField,
+            imageButton,
+            confirmButton
+        );
+        
+        // Create Add Food Item Window
+        Stage stage = new Stage();
+        Scene scene = new Scene(vBox, 300, 300);
+        stage.setScene(scene);
+        stage.setTitle("Add Food Item");
+        stage.show();
     }
     
     public static void main(String[] args)
