ab01d5be374ed525b94cfc8ae3c4bb13c17eadda
eltwento <trentbuckallew@gmail.com>
2022-03-22 22:19:24 -0700

    lots of classes
    User class
    Customer class
    CustomerConfigurations class
    PaymentInformation class

---
+++ src/main/java/com/Team12/FoodAppProject/Customer.java
@@ -0,0 +1,7 @@
+package com.Team12.FoodAppProject;
+
+import java.util.ArrayList;
+
+public class Customer{
+    ArrayList<Order> orderHistory = new ArrayList<Order>;
+}
--- src/main/java/com/Team12/FoodAppProject/User.java
+++ src/main/java/com/Team12/FoodAppProject/User.java
@@ -1,5 +1,8 @@
 package com.Team12.FoodAppProject;
 
-public interface User {
-    public boolean verifyLogin(String username, String password);
+public abstract class User {
+
+    private String username, password;
+
+    public abstract boolean verifyLogin(String username, String password);
 }
